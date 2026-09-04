968f252029ddaea1a61a1a40f037c0b401526f18
senacmatheus2026-cell <senac.matheus2026@gmail.com>
2026-09-04 17:14:39 -0400

    add declaração-var

--- declaração-var.js
+++ declaração-var.js
@@ -1,7 +1,10 @@
 function exemploVar() {
+console.log(x);
+
+
 var x = 10;
 if (true) {
-    var x = 20; // mesma variével x é redeclarada dentro do bloco
+    var x = 20; // mesma variavel x e redeclarada dentro do bloco
     console.log(x); // 20
 }
 console.log(x); // 20 (o valor foi alterado)
