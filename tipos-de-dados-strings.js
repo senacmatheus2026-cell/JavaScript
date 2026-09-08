02be3a0d9c4b305757c5cf244d7ecec006868b25
senacmatheus2026-cell <senac.matheus2026@gmail.com>
2026-09-08 17:09:48 -0400

    add tipos-de-dados-strings

---
+++ tipos-de-dados-strings.js
@@ -0,0 +1,5 @@
+function exemploString () {
+    let nome = "Ana"; // String com aspas duplas
+    let sobrenome = 'Silva'; // string com aspas simples
+    let saudacao = `Olá, ${nome}! `; // template literal com interpolação de variáveis
+}
