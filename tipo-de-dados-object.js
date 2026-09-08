5fe36e9572ee24331e6ec44bd0eddaea8a71b0f3
senacmatheus2026-cell <senac.matheus2026@gmail.com>
2026-09-08 17:11:25 -0400

    add tipos-de-dados-Object

---
+++ tipo-de-dados-object.js
@@ -0,0 +1,10 @@
+function exemploObject () {
+    let pessoa = {
+        nome: "Carlos"
+        idade: 28,
+        casado: true,
+    };
+    // acessando propriedades do objeto
+    console.log(pessoa.nome); //"Carlos"
+    console.log(pessoa.idade); // 280   
+}
