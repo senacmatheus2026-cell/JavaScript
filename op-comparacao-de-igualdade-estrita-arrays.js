ba4e849aca5b5c92d4bfef4b876ebdef7e970259
senacmatheus2026-cell <senac.matheus2026@gmail.com>
2026-09-15 18:07:53 -0300

    add comparacao-de-igualdade-estrita-arrays.js

---
+++ op-comparacao-de-igualdade-estrita-arrays.js
@@ -0,0 +1,10 @@
+//listaA é um array com 5 elementos
+let listaA = [10, 20, 30, 40, 50];
+
+// listaB é outro array com o mesmos 5 elementos que a listaA
+let listaB = [10, 20, 30, 40, 50];
+
+// listaC recebe uma copia da listaA
+let listaC = listaA;
+
+console.log(listaA === listaC); // true
