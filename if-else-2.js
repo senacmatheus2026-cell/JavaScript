42262520c3565431e5ebc28fcd2e463d3f0e24f9
raphael <senac.matheus2026@gmail.com>
2026-09-22 15:20:30 -0300

    add if-elfe-2.js

---
+++ if-else-2.js
@@ -0,0 +1,7 @@
+let diaDaSemana = "quarta-feira";
+
+if (diaDaSemana === "sabado" || diaDaSemana === "domingo") {
+    console.log("e fim de semana!");
+} else {
+    console.log("e dia de semana.");
+}
