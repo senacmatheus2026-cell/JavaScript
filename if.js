0b70f8acbb3e6bdc9e8903d3a22be878183c8f44
raphael <senac.matheus2026@gmail.com>
2026-09-22 15:11:34 -0300

    add if.js

---
+++ if.js
@@ -0,0 +1,7 @@
+let hora = 15;
+
+if (hora < 12) {
+    console.log("Bom dia!");
+}
+// o restaurante do codigo continua independentemente se a condicao for verdadeira ou nao.
+console.log("Dinheiro nao traz felicidade. Me de o seu e seja feliz!"); 
